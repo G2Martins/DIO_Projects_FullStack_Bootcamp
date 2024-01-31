e8384835b669a2e91741520e53217d1a4e3c96af
G2Martins <gustavogripaldi@outlook.com>
2024-01-31 15:37:09 -0300

    docs: add JS files

---
+++ JavaScript/Aula2/script.js
@@ -0,0 +1,11 @@
+// Tipos primitivos
+
+// Boolean
+var VouF = false;
+console.log(VouF);
+console.log(typeof(VouF)); // Identifica o tipo da variável
+
+// Number
+var number_1 = 1;
+console.log(number_1);
+console.log(typeof(number_1));
