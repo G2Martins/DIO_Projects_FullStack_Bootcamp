3b6dc1ae29158d138a7d5d0123d00a59d792b46d
G2Martins <gustavogripaldi@outlook.com>
2024-02-23 17:37:14 -0300

    Pokedex

--- JavaScript/Desafios/Pokedex/assets/js/main.js
+++ JavaScript/Desafios/Pokedex/assets/js/main.js
@@ -1,27 +1,34 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 12
+let offset = 0;
 
-function convertPokemonToLi(pokemon){
-    return `
+function loadPokemonItens(offset, limit){                                                
+    
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {                    
+        const newHTML = pokemons.map((pokemon) =>
+        `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             
             <div class="detail">
                 <ol class="types">
-                   ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                   ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
                 
                 <img src="${pokemon.photo}" 
                     alt="${pokemon.name}">
             </div>
-        </li>
-    `
-}
+        </li>                                                              
+        `).join('')                                                   // Promise - A promessa de um Response(Resposta)  
+        pokemonList.innerHTML += newHTML                              // Processamento Assíncrono (Não tem a resposta de Imediato)  
+    })                                                                // Transforma o Objeto em um item de Lista em HTML                 
+}                                                                     // Juntar todos os elementos da Lista Convertendo em uma string a partir de um separador
+   
+loadPokemonItens(offset, limit)
 
-const pokemonList = document.getElementById('pokemonList')
-
-// Promise - A promessa de um Response(Resposta) 
-// Processamento Assíncrono (Não tem a resposta de Imediato)
-pokeApi.getPokemons().then((pokemons = []) => {   
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('') // Transforma o Objeto em um item de Lista em HTML
-})                                                                     // Juntar todos os elementos da Lista Convertendo em uma string a partir de um separador                
-
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
