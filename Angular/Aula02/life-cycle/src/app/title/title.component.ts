95944b61ae6f66f3093d38ba42b076d5c7b08f20
G2Martins <gustavogripaldi@outlook.com>
2024-08-16 03:27:32 -0300

    add: lifeCycle Hooks Angular files

---
+++ Angular/Aula02/life-cycle/src/app/title/title.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnChanges, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.css']
+})
+
+export class TitleComponent implements OnInit, OnChanges {
+  
+  @Input() nome:string = ''
+  
+  constructor() { 
+    console.log("Construtor")
+  }
+  
+  ngOnChanges(): void {
+    console.log("Onchange")
+  }
+
+  ngOnInit(): void {
+    console.log("OnInit") 
+  }
+
+  
+
+}
